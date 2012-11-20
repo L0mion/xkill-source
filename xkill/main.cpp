ac074d7152ae8be5961fa68e81bd913c6f30af06
L0mion <davidgrelsson@gmail.com>
2012-11-20 17:06:57 +0100

    Added simple exception handling

--- xkill/main.cpp
+++ xkill/main.cpp
@@ -1,12 +1,16 @@
 #include <Windows.h>
 #include <windowsx.h>
-
-#include <xkill-renderer\renderingComponent.h>
-#include <xkill-renderer\window.h>
+#include <tchar.h>
+#include <xkill-renderer/renderingComponent.h>
+#include <xkill-renderer/cameraBasic.h>
+#include "window.h"
 
 #if defined( DEBUG ) || defined( _DEBUG )
 #include <vld.h>
 #endif
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int cmdShow);
+void run(RenderingComponent* renderingComponent, Window* window, CameraBasic* camera);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int cmdShow)
 {
@@ -21,18 +25,31 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		screenWidth, 
 		screenHeight, 
 		1);
-	renderingComponent->init();
+	HRESULT hr = renderingComponent->init();
 
-	while(window->getIsActive())
-	{
-		window->checkMessages();
-		//renderingComponent->render();
-	}
+	float aspect = (float)screenWidth/(float)screenHeight;
+	CameraBasic* camera = new CameraBasic(aspect, 0.78f, 1000.0f, 0.1f, screenWidth, screenHeight);
+	camera->updateProj();
 
+	if(SUCCEEDED(hr))
+		run(renderingComponent, window, camera);
+
+	delete camera;
 	renderingComponent->cleanUp();
 	delete renderingComponent;
 	delete window;
 
-
 	return 0;
+}
+
+void run(RenderingComponent* renderingComponent, Window* window, CameraBasic* camera)
+{
+	while(window->getIsActive())
+	{
+		window->checkMessages();
+
+		//move cam
+		camera->updateView();
+		renderingComponent->render(camera->getView(), camera->getProjection());
+	}
 }
