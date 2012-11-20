f1eafca14fdf7afa7a36ae4bdc515049081ec2fa
L0mion <davidgrelsson@gmail.com>
2012-11-20 17:02:17 +0100

    Interface to force cleanUp-functions onto every class containing COM

---
+++ xkill-renderer/d3dInterface.h
@@ -0,0 +1,10 @@
+#ifndef XKILL_RENDERER_D3DINTERFACE_H
+#define XKILL_RENDERER_D3DINTERFACE_H
+
+class D3DInterface
+{
+public:
+	virtual void cleanUp()	= 0;
+};
+
+#endif //XKILL_RENDERER_D3DINTERFACE_H
