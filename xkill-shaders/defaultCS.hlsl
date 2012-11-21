494ae92222bcec490689fc029a76c433227839a0
L0mion <davidgrelsson@gmail.com>
2012-11-21 16:55:25 +0100

    Added default compute shader

---
+++ xkill-shaders/defaultCS.hlsl
@@ -0,0 +1,14 @@
+RWTexture2D<float4> output : register(u0);
+
+Texture2D gBufferNormal		: register( t0 );
+Texture2D gBufferAlbedo		: register( t1 );
+
+SamplerState ss : register(s0);
+
+[numthreads(32, 32, 1)]
+void main( uint3 threadID : SV_DispatchThreadID )
+{
+	float2 texCoord = float2(threadID.x/800.0f, threadID.y/800.0f);
+	float4 color = gBufferAlbedo.SampleLevel(ss, texCoord, 0);
+	output[threadID.xy] = color;
+}
