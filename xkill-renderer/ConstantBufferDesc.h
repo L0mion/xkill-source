0f41bf2995b1ae9042925d37348bd5f528cc08a6
L0mion <davidgrelsson@gmail.com>
2012-11-22 17:06:13 +0100

    Renamed ConstantBuffers.h to ConstantBufferDesc.h

---
+++ xkill-renderer/ConstantBufferDesc.h
@@ -0,0 +1,43 @@
+#ifndef XKILL_RENDERER_CONSTANTBUFFERS_H
+#define XKILL_RENDERER_CONSTANTBUFFERS_H
+
+#include "mathBasic.h"
+
+/*! \defgroup xkill-renderer-constant-buffers xkill-renderer-constant-buffers
+	\ingroup xkill-renderer
+	Constant buffers transferring data to shaders in batches. */
+
+//! Per-Frame constant buffer
+/*!
+Contains data that will be sent to shader every frame.
+\ingroup xkill-renderer-constant-buffers
+*/
+struct CBPerFrameDesc
+{
+	MatF4 worldViewProj_; //!< Final matrix.
+};
+//! Size of Per-Frame constant buffer. Must have a size of a multiple of 16 bytes.
+/*!
+\ingroup xkill-renderer-constant-buffers
+*/
+static const unsigned int cbPerFrameSize = 64;
+
+//! Per-Instance constant buffer
+/*!
+Contains data that will be sent to shader for every new instance.
+\ingroup xkill-renderer-constant-buffers
+*/
+struct CBPerInstanceDesc
+{
+	unsigned int screenWidth_;
+	unsigned int screenHeight_;
+	unsigned int tileWidth_;
+	unsigned int tileHeight_;
+};
+//! Size of Per-Instance constant buffer. Transfers data only changed during initialization to shader. Must have a size of a multiple of 16 bytes.
+/*!
+\ingroup xkill-renderer-constant-buffers
+*/
+static const unsigned int cbPerInstanceSize = 64;
+
+#endif //XKILL_RENDERER_CONSTANTBUFFER_H
