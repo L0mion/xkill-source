0b63aa31019c3eb9eac5ab0dd97d489b2ec79438
CaterHatterPillar <CaterHatterPillar@gmail.com>
2012-11-23 13:47:23 +0100

    Made vertex-type into template

--- xkill-renderer/MeshGeometry.h
+++ xkill-renderer/MeshGeometry.h
@@ -1,18 +1,42 @@
 #ifndef XKILL_RENDERER_MESHGEOMETRY_H
 #define XKILL_RENDERER_MESHGEOMETRY_H
 
+#include <vector>
+
+#include <vertices.h>
+
+template <typename Vertex>
 class MeshGeometry
 {
 public:
-	MeshGeometry();
-	~MeshGeometry();
+	MeshGeometry(
+		const std::vector<Vertex>			mgVertices,
+		const std::vector<unsigned int>		mgIndices)
+	{
+		mgVertices_ = mgVertices;
+		mgIndices_	= mgIndices;
+	}
+	~MeshGeometry()
+	{
+		//Do nothing.
+	}
+
+	const std::vector<Vertex>			getMGVertices()
+	{
+		return mgVertices_;
+	}
+	const std::vector<unsigned int>		getMGIndices()
+	{
+		return mgIndices_;
+	}
 protected:
 private:
+	std::vector<Vertex>				mgVertices_;
+	std::vector<unsigned int>		mgIndices_;
 };
 
 /*
-ToImplement:
-* Is to hold vertex data
+Template vertex-type?
 */
 
 #endif //XKILL_RENDERER_MESHGEOMETRY_H
