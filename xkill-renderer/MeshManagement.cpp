902959f8d5348edc96d5da062a97b496be15df42
CaterHatterPillar <CaterHatterPillar@gmail.com>
2012-11-30 16:33:34 +0100

    New classes to manage meshes in Renderer

---
+++ xkill-renderer/MeshManagement.cpp
@@ -0,0 +1,21 @@
+#include <MeshManagement.h>
+
+MeshManagement::MeshManagement()
+{
+
+}
+MeshManagement::~MeshManagement()
+{
+
+}
+
+HRESULT MeshManagement::init()
+{
+	HRESULT hr = S_OK;
+
+	return hr;
+}
+void MeshManagement::reset()
+{
+
+}
