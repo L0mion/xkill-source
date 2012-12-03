eb5d417604e69da38ba4de88ea89d5574c8e5c89
L0mion <davidgrelsson@gmail.com>
2012-12-03 09:41:10 +0100

    Changed Default Samplerstate. The default sampler state now uses adress clamp instead of adress wrap to avoid artefacts on the side of the screen.

--- xkill-renderer/renderingComponent.cpp
+++ xkill-renderer/renderingComponent.cpp
@@ -480,9 +480,9 @@ HRESULT RenderingComponent::initSSDefault()
 	D3D11_SAMPLER_DESC sampDesc;
     ZeroMemory(&sampDesc, sizeof(sampDesc));
 	sampDesc.Filter		= D3D11_FILTER_MIN_MAG_MIP_LINEAR;
-    sampDesc.AddressU	= D3D11_TEXTURE_ADDRESS_WRAP;
-    sampDesc.AddressV	= D3D11_TEXTURE_ADDRESS_WRAP;
-    sampDesc.AddressW	= D3D11_TEXTURE_ADDRESS_WRAP;
+    sampDesc.AddressU	= D3D11_TEXTURE_ADDRESS_CLAMP;
+    sampDesc.AddressV	= D3D11_TEXTURE_ADDRESS_CLAMP;
+    sampDesc.AddressW	= D3D11_TEXTURE_ADDRESS_CLAMP;
     sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
     sampDesc.MinLOD		= 0;
     sampDesc.MaxLOD		= D3D11_FLOAT32_MAX;
