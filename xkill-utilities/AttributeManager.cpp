350b292f56b3a87d01cc25e1ed819009a3edbfa4
Silktoid <Silktoid@gmail.com>
2013-01-11 08:52:39 +0100

    Minor changes.

--- xkill-utilities/AttributeManager.cpp
+++ xkill-utilities/AttributeManager.cpp
@@ -6,26 +6,26 @@ AttributeManager::AttributeManager()
 	entities = new EntityStorage;
 	settings_entity = entities->createEntity();
 
-	position		.init	(ATTRIBUTE_POSITION);
-	spatial			.init	(ATTRIBUTE_SPATIAL);
-	render			.init	(ATTRIBUTE_RENDER);
-	debugShape		.init	(ATTRIBUTE_DEBUGSHAPE);
-	physics			.init	(ATTRIBUTE_PHYSICS);
-	camera			.init	(ATTRIBUTE_CAMERA);
-	input			.init	(ATTRIBUTE_INPUT);
-	input			.init	(ATTRIBUTE_INPUTDEVICESETTINGS);
-	player			.init	(ATTRIBUTE_PLAYER);
-	bounding		.init	(ATTRIBUTE_BOUNDING);
-	projectile		.init	(ATTRIBUTE_PROJECTILE);
-	lightDir		.init	(ATTRIBUTE_LIGHT_DIRECTIONAL);
-	lightPoint		.init	(ATTRIBUTE_LIGHT_POINT);
-	lightSpot		.init	(ATTRIBUTE_LIGHT_SPOT);
-	mesh			.init	(ATTRIBUTE_MESH);
-	health			.init	(ATTRIBUTE_HEALTH);
-	damage			.init	(ATTRIBUTE_DAMAGE);
-	spawnPoint		.init	(ATTRIBUTE_SPAWNPOINT);
-	weaponStats		.init	(ATTRIBUTE_WEAPONSTATS);
-	explosionSphere	.init	(ATTRIBUTE_EXPLOSIONSPHERE);
+	position			.init	(ATTRIBUTE_POSITION);
+	spatial				.init	(ATTRIBUTE_SPATIAL);
+	render				.init	(ATTRIBUTE_RENDER);
+	debugShape			.init	(ATTRIBUTE_DEBUGSHAPE);
+	physics				.init	(ATTRIBUTE_PHYSICS);
+	camera				.init	(ATTRIBUTE_CAMERA);
+	input				.init	(ATTRIBUTE_INPUT);
+	inputDeviceSettings	.init	(ATTRIBUTE_INPUTDEVICESETTINGS);
+	player				.init	(ATTRIBUTE_PLAYER);
+	bounding			.init	(ATTRIBUTE_BOUNDING);
+	projectile			.init	(ATTRIBUTE_PROJECTILE);
+	lightDir			.init	(ATTRIBUTE_LIGHT_DIRECTIONAL);
+	lightPoint			.init	(ATTRIBUTE_LIGHT_POINT);
+	lightSpot			.init	(ATTRIBUTE_LIGHT_SPOT);
+	mesh				.init	(ATTRIBUTE_MESH);
+	health				.init	(ATTRIBUTE_HEALTH);
+	damage				.init	(ATTRIBUTE_DAMAGE);
+	spawnPoint			.init	(ATTRIBUTE_SPAWNPOINT);
+	weaponStats			.init	(ATTRIBUTE_WEAPONSTATS);
+	explosionSphere		.init	(ATTRIBUTE_EXPLOSIONSPHERE);
 }
 
 AttributeManager::~AttributeManager()
