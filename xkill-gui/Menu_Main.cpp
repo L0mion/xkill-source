b19db399a30238664841c7440fc9bf35b49c95d8
Nils Forsman <nils@nissefors.se>
2013-02-07 14:44:00 +0100

    Fixed stylesheets and begun character change

--- xkill-gui/Menu_Main.cpp
+++ xkill-gui/Menu_Main.cpp
@@ -32,17 +32,17 @@ Menu_Main::Menu_Main( QWidget* parent ) : QMainWindow(parent), ToggleHelper(this
 	//setAttribute(Qt::WA_TranslucentBackground);
 	setWindowFlags(Qt::WindowStaysOnTopHint);
 
-	ui.verticalLayout_2->setSpacing(0);
-	ui.verticalLayout_2->setMargin(0);
-	ui.verticalLayout->setSpacing(0);
-	ui.verticalLayout->setMargin(0);
+	//ui.verticalLayout_2->setSpacing(0);
+	//ui.verticalLayout_2->setMargin(0);
+	//ui.verticalLayout->setSpacing(0);
+	//ui.verticalLayout->setMargin(0);
 
-	connect(ui.pushButton_exit,			SIGNAL(clicked()),					this,	SLOT(slot_quitToDesktop()));
+	connect(ui.pushButton_exit_5,		SIGNAL(clicked()),					this,	SLOT(slot_quitToDesktop()));
 	connect(ui.pushButton_exit_2,		SIGNAL(clicked()),					this,	SLOT(slot_quitToDesktop()));
 	connect(ui.comboBox_LevelSelect,	SIGNAL(currentIndexChanged(int)),	this,	SLOT(slot_selectLevel(int)));
-	connect(ui.pushButton_AddLevel,		SIGNAL(clicked()),					this,	SLOT(slot_addLevel()));
-	connect(ui.pushButton_SaveLevel,	SIGNAL(clicked()),					this,	SLOT(slot_saveLevel()));
-	connect(ui.pushButton_RemoveLevel,	SIGNAL(clicked()),					this,	SLOT(slot_removeLevel()));
+	//connect(ui.pushButton_AddLevel,		SIGNAL(clicked()),					this,	SLOT(slot_addLevel()));
+	//connect(ui.pushButton_SaveLevel,	SIGNAL(clicked()),					this,	SLOT(slot_saveLevel()));
+	//connect(ui.pushButton_RemoveLevel,	SIGNAL(clicked()),					this,	SLOT(slot_removeLevel()));
 	connect(ui.pushButton_startGame,	SIGNAL(clicked()),					this,	SLOT(slot_startGame()));
 
 	/*
@@ -156,9 +156,9 @@ void Menu_Main::loadXML()
 		QStandardItem* child = allLevelItm->child(i,0);
 		ui.comboBox_LevelSelect->addItem(child->text());
 	}
-	ui.treeView->setModel(levelListModel);
+//	ui.treeView->setModel(levelListModel);
 
-	ui.treeView->setExpanded(allLevelItm->index(), true);
+//	ui.treeView->setExpanded(allLevelItm->index(), true);
 	
 }
 
@@ -184,9 +184,9 @@ void Menu_Main::slot_addLevel()
 
 void Menu_Main::slot_removeLevel()
 {
-	QStandardItem* levels = levelListModel->item(0,0);
-	QModelIndex index = ui.treeView->currentIndex();
-	levels->removeRow(index.row());
+//	QStandardItem* levels = levelListModel->item(0,0);
+//	QModelIndex index = ui.treeView->currentIndex();
+//	levels->removeRow(index.row());
 }
 
 void Menu_Main::slot_saveLevel()
