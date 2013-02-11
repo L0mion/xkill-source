65e2cd2d32bf400cd15c3b1b2502de411929f0b5
L0mion <davidgrelsson@gmail.com>
2013-02-11 17:01:05 +0100

    Added attribute type for animations.

--- xkill-utilities/AttributeType.cpp
+++ xkill-utilities/AttributeType.cpp
@@ -1030,6 +1030,15 @@ DataItemList* Attribute_Ray::getDataList()
 	return list;
 }
 
+Attribute_Animation::Attribute_Animation()
+{
+	animationID = -1;
+	time = 0.0f;
+}
+Attribute_Animation::~Attribute_Animation()
+{
+}
+
 void Attribute_Ray::saveTo( DataItemList* list )
 {
 	list->get(&from);
@@ -1116,3 +1125,4 @@ DataItemList* Attribute_Bounding::getDataList()
 	list->add_NotSupported("bonvexPoints");
 	return list;
 }
+
