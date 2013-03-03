8821569a6a16f01acc43a4fb2dd73f818605c31a
Adelost <adelost@gmail.com>
2013-03-03 22:36:35 +0100

    Added new font

--- xkill-gui/Menu_Main2.cpp
+++ xkill-gui/Menu_Main2.cpp
@@ -168,7 +168,7 @@ void Menu_Main2::mousePressEvent( QMouseEvent *e )
 void Menu_Main2::loadCustomFonts()
 {
 	QStringList list;
-	list << "arcade_interlaced.ttf" << "arcade_rounded.ttf" << "arista_light.ttf" << "digital_7_mono.ttf";
+	list << "arcade_interlaced.ttf" << "arcade_rounded.ttf" << "arista_light.ttf" << "digital_7_mono.ttf" << "angies_new_house.ttf";
 	int fontID(-1);
 	bool fontWarningShown(false);
 	for (QStringList::const_iterator constIterator = list.constBegin(); constIterator != list.constEnd(); ++constIterator) 
