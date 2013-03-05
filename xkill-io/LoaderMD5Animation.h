81082445333eaffa51096d17805cc594d11b70d6
L0mion <davidgrelsson@gmail.com>
2013-03-05 17:02:35 +0100

    Renamed a file

--- xkill-io/LoaderMD5Animation.h
+++ xkill-io/LoaderMD5Animation.h
@@ -1,10 +1,29 @@
 #ifndef XKILL_IO_LOADER_MD5_ANIMATION_H
 #define XKILL_IO_LOADER_MD5_ANIMATION_H
 
+#include <vector>
+
+#include "LoaderMD5JointInfo.h"
+#include "LoaderMD5BoundsDesc.h"
+#include "LoaderMD5BaseFrameDesc.h"
+#include "LoaderMD5FrameDataDesc.h"
+#include "LoaderMD5FrameSkeletonDesc.h"
+
 class LoaderMD5Animation
 {
 public:
 private:
+	
+
+	int md5Version_;
+	int numFrames_;
+	int numJoints_;
+	int frameRate_;
+	int numAnimatedComponents_;
+
+	float animationDuration_;
+	float frameDuration_;
+	float animationTime_;
 };
 
 #endif //XKILL_IO_LOADER_MD5_ANIMATION_H
--- xkill-io/LoaderMD5BoundsDesc.h
+++ xkill-io/LoaderMD5BoundsDesc.h
@@ -1,19 +1,19 @@
-#ifndef XKILL_IO_LOADER_MD5_BOUNDING_BOX_DESC_H
-#define XKILL_IO_LOADER_MD5_BOUNDING_BOX_DESC_H
+#ifndef XKILL_IO_LOADER_MD5_BOUNDS_DESC_H
+#define XKILL_IO_LOADER_MD5_BOUNDS_DESC_H
 
 #include <DirectXMath.h>
 
-struct LoaderMD5BoundingBoxDesc
+struct LoaderMD5BoundsDesc
 {
 	DirectX::XMFLOAT3 min_;
 	DirectX::XMFLOAT3 max_;
 
-	LoaderMD5BoundingBoxDesc()
+	LoaderMD5BoundsDesc()
 	{
 		min_ = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f);
 		max_ = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f);
 	}
-	LoaderMD5BoundingBoxDesc(DirectX::XMFLOAT3 min, DirectX::XMFLOAT3 max)
+	LoaderMD5BoundsDesc(DirectX::XMFLOAT3 min, DirectX::XMFLOAT3 max)
 	{
 		min_ = min;
 		max_ = max;
